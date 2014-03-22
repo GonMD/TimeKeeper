89d3c25ce7d12a8d9e3ee407181d5e5aab8b0e86
GonMD <drgon47@gmail.com>
2014-03-22 11:04:09 -0500

    Commit 2
    removed pickle & sqlalcehmy imports, added sqlite3 import. added a menu
    option but with only test output when selected. Started working on
    sending id, name, and date of birth to a sqlite DB.

--- TimeClockApp.py
+++ TimeClockApp.py
@@ -1,9 +1,14 @@
 import os
 import sys
-import sqlalchemy.0.9.3
-import pickle
+import sqlite3
 import datetime
 
+conn = sqlite3.connect('employeeDatabase.db')
+
+c = conn.cursor()
+
+c.execute('''CREATE TABLE IF NOT EXISTS users(
+    id INTEGER PRIMARY KEY, name, dob)''')
 
 answer = True
 while answer:
@@ -14,7 +19,8 @@ while answer:
     3. View Hours
     4. Add Employee
     5. List Employees
-    6. Exit application
+    6. Administrator
+    7. Exit application
 """)
 
     answer = input("Select an option.")
@@ -31,26 +37,34 @@ while answer:
 
     elif answer == "3":
         print("\n Hours: X")
-        os.system('CLS')
 
     elif answer == "4":
         nameFirst = input("\n Enter first name: ")
         nameMiddle = input("\n Enter middle name: ")
         nameLast = input("\n Enter last name: ")
-        dateOfBirth = input("\n Enter Date of Birth, eg. 4/22/1987: ")
+        
+        dateOfBirth = (input("\n Enter Date of Birth, eg. 4/22/1987: "))
+        
+        
+        
         newEmployee = str(nameLast + ", " + nameFirst + " " + nameMiddle)
 
-        employeeDatabase = { newEmployee }
+        c.execute("INSERT INTO users (name) VALUES ('" + newEmployee + "');" )
+       
+        conn.commit()
 
-        #pickle.dump( employeeDatabase, open("employeeDatabase.p", "wb") )
+        
 
         print("\n New employee added: " + nameLast + ", " + nameFirst + " " + nameMiddle + ", born on " + dateOfBirth)
         
     elif answer == "5":
         print (newEmployee)
 
-
     elif answer == "6":
+        print ("Test")
+
+    
+    elif answer == "7":
         sys.exit()
 
     else:
